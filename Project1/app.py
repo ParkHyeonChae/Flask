81b8090817909ac29e9b741578e8508d256a1199
Park Hyeon Chae <park19996@naver.com>
2019-08-01 03:14:18 +0900

    commit

--- Project1/app.py
+++ Project1/app.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, request
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def main():
-    return render_template('main.html')
+	return render_template('index.html')
 
-@app.route('/state/<button>')
-def state(button):
-    return render_template('state.html',btn_state=button)
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+	app.run(debug= True)
