2672b67992878a362662a2eb0ed2f06f659013b9
Park Hyeon Chae <park19996@naver.com>
2019-08-01 17:36:53 +0900

    commit

--- Project1/app.py
+++ Project1/app.py
@@ -1,11 +1,15 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def main():
 	return render_template('index.html')
+
+@app.route('/facebook')
+def facebook():
+	return redirect('http://facebook.com')
 
 if __name__ == "__main__":
 	app.run(debug= True)
